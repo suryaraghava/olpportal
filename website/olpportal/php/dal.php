7bafee075bac706c186d62d1496d66347380c689
NLNRao <nellutlalnrao@gmail.com>
2015-05-16 21:34:26 +0530

    Added Course Class for course. courselinks table

--- website/olpportal/php/dal.php
+++ website/olpportal/php/dal.php
@@ -7,7 +7,7 @@ require 'database.php';
  * and open the template in the editor.
  */
 
-class useraccounts
+class UserAccounts
 {
     /* USER REGISTRATION FORM */
     /* Account creation : Insertion */
@@ -115,7 +115,46 @@ class useraccounts
             }
 }
 
-$acc=new useraccounts();
+
+class Courses
+{
+    function addCourses($courseName, $courseNum)
+    /* Adding Courses and It returns Primary Key */
+    {
+        $dbObj=new InteractDatabase();
+        $isql="INSERT INTO `courses`(`courseName`, `courseNumber`) VALUES ('".$courseName."',".$courseNum.")";
+        $gsql="SELECT * FROM `courses` WHERE `courseName`='".$courseName."' AND `courseNumber`=".$courseNum.";";
+        
+        $dbObj->addupdateData($isql);
+        $json=$dbObj->getJSONData($gsql);
+        $dejson=json_decode($json);
+        $courseId=$dejson[0]->{'idCourses'};
+        
+        return $courseId;
+    }
+    
+    function addCourseLink($courseId, $courseVideo, $coursePDF)
+    {
+        $dbObj=new InteractDatabase();
+        
+        $sql="INSERT INTO `courselinks`(`courseID`, `courseVideoLink`, `coursePDFLink`) ";
+        $sql.="VALUES (".$courseId.",'".$courseVideo."','".$coursePDF."')";
+        
+        $dbObj->addupdateData($sql);
+    }
+    
+    function viewCourseDetails()
+    {
+        $sql="SELECT * FROM `courses`,`courselinks` WHERE courses.idCourses=courselinks.courseID";
+        $dbObj=new InteractDatabase();
+        $json=$dbObj->getJSONData($sql);
+        
+        echo $json;
+    }
+    
+    
+}
+$acc=new UserAccounts();
 
 /* Get Registration */
 // $acc->getRegister("Test", "password", "firstName", "lastName", "1234", "mobile", "abcd", "designation");
