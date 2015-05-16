df70ddd1432bfc5e0a876d6347dc1acdf0874f9d
NLNRao <nellutlalnrao@gmail.com>
2015-05-16 18:52:03 +0530

    PHP-DBIntegrity File and Constants defining File

---
+++ website/olpportal/php/define.php
@@ -0,0 +1,12 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/* DATABASE CREDIENTIALS */
+define("SERVER_NAME","localhost:3306", true);
+define("DB_NAME","olpportal", true);
+define("DB_USER","root", true);
+define("DB_PASSWORD","@anupanup123", true);
